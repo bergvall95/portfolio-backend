fedf00ccbe44c9fde47c0fb7403f177eca998a1d
alex bergvall <bergvall95@gmail.com>
2022-12-06 17:38:45 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var { config, app } = require("./server");
+
+var port = process.env.PORT || config.port || 9999;
+
+app.listen(port, null, function (err) {
+  console.log("Alex Gatekeeper/portfolio backend: http://localhost:" + port);
+});
